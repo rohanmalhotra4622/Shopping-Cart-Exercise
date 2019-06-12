e662ee748a8f61ca881c186391923ce0d12f5acd
rohanmalhotra4622 <44009676+rohanmalhotra4622@users.noreply.github.com>
2019-06-11 21:18:19 -0400

    Seperating info input and output process

--- shopping_cart.py
+++ shopping_cart.py
@@ -41,25 +41,24 @@ products = [
 #        break
 
 total_price = 0
-
+selected_ids = []
 
 while True:
     selected_id = input('Please input a product identifier:')  
     if selected_id == 'DONE':
         break
     else:
-        matching_products = [p for p in products if p['id'] == int(selected_id) ]
-        matching_product = matching_products[0]
-        total_price = total_price + matching_product['price']
-        print("SELECTED PRODUCT:  " + matching_product['name'] + ' ' + str(matching_product['price']))
-
+        selected_ids.append(selected_id)
 
 # Information capture output
+#print(selected_ids)
 
+for selected_id in selected_ids:
+    matching_products = [p for p in products if str(p['id']) == str(selected_id)]
+    matching_product = matching_products[0]
+    total_price = total_price + matching_product['price']
+    print('SELECTED PRODUCT: ' + matching_product['name'] + ' ' + str(matching_product['price']))
 print('TOTAL PRICE: ' + str(total_price))
-
-
-
 
 #Desired Output
 
